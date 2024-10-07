40aa15df35abe9ce810374b4a7b22b7099518fde
aftab <shaikaftab>
2024-10-07 18:11:27 +0530

    step 2 css done

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,7 @@
+import "./chatList";
+const ChatList = () => {
+    return (
+    <div className="chatList">ChatList</div>
+)
+}
+export default ChatList 
---
+++ src/components/list/userinfo/Userinfo.jsx
@@ -0,0 +1,18 @@
+import "./userinfo.css"
+const UserInfo =()=>{
+    return (
+        <div className='userInfo'>
+            <div className="user">
+           <img src="./avatar.png"/>
+           <h2>aftab shaik</h2>
+            </div>
+            <div className="icons">
+                <img src="./more.png"/>
+                <img src="./video.png"/>
+                <img src="./edit.png"/>
+                </div>
+           UserInfo
+        </div>
+    )
+}
+export default UserInfo
